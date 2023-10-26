c8168d12f8efe5b407f7186a83d693fb2ad87334
Tavinho75 <128919525+Tavinho75@users.noreply.github.com>
2023-10-26 16:51:49 -0300

    Novos Execícios

---
+++ Aula11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) { // CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija semre usando cinto d segurança`)
