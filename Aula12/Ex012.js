6c3fd9b35665bb757ee80bda78c7ffd003f562c1
Tavinho75 <128919525+Tavinho75@users.noreply.github.com>
2023-11-02 15:55:42 -0300

    Dois exercícios adicionais

---
+++ Aula12/Ex012.js
@@ -0,0 +1,10 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas.`)
+if (hora > 6 && hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
